3bcf00ea9e8d4c961dc17dd3955bc3a7b3c3a986
c <cslemon96@gmail.com>
2022-09-25 15:39:12 +0200

    feat(PWA): Add PWA setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const runtimeCaching = require('next-pwa/cache')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  }
   images: {
     domains: ['t2.gstatic.com', 'www.google.com', 'google.com']
   }
