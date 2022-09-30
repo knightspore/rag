5c920da081f06539006ec3aac52198ad0a189290
Ciaran <cslemon96@gmail.com>
2022-09-30 02:28:05 +0200

    fix(parse): Return entries correctly on feed response

--- parse/parse.go
+++ parse/parse.go
@@ -14,6 +14,23 @@ type Feed struct {
 	JSON []byte
 }
 
+type FeedXML struct {
+	Title       string       `xml:"title" json:"title"`
+	Description string       `xml:"description" json:"description"`
+	URL         string       `xml:"link" json:"url"`
+	LastUpdated string       `xml:"updated" json:"lastUpdated"`
+	Articles    []EntriesXML `xml:"entry" json:"articleList"`
+}
+
+type EntriesXML struct {
+	PubDate     string `xml:"pubDate" json:"pubDate"`
+	PubEpoch    int64  `xml:"pubEpoch" json:"pubEpoch"`
+	Title       string `xml:"title" json:"title"`
+	Description string `xml:"description" json:"description"`
+	Content     string `xml:"content" json:"content"`
+	URL         string `xml:"link" json:"url"`
+}
+
 func NewFeed(url string) (Feed, error) {
 
 	doc, err := Fetch(url)
