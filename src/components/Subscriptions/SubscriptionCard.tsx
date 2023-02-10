a9614f94e33c6f1cc0d22200c4e1f5a46936eb42
Ciaran <cslemon96@gmail.com>
2023-02-10 02:26:30 +0200

    feat(SubscriptionCard): Add Modal for Subscription Deletion

--- src/components/Subscriptions/SubscriptionCard.tsx
+++ src/components/Subscriptions/SubscriptionCard.tsx
@@ -6,6 +6,8 @@ import {useFilterContext} from '../Providers/FilterContextProvider';
 import {IoTrashSharp} from 'react-icons/io5';
 import Icon from '../Icon';
 import {useAppContext} from '../Providers/AppContextProvider';
+import {useState} from 'react';
+import {Dialog} from '@headlessui/react';
 
 type Props = {sub: Partial<Subscriptions>; remove: (title?: string) => void};
 
@@ -45,7 +47,10 @@ export default function SubscriptionCard({sub}: Props) {
         }
     }
 
+    const [deletePromptOpen, setDeletePromptOpen] = useState(true)
+
     return (
+      <>
         <div
             className={`select-none flex items-center p-1 px-2 gap-2 card ${
                 filtered && !selected && 'opacity-50'
@@ -56,12 +61,24 @@ export default function SubscriptionCard({sub}: Props) {
                 <Icon src={sub.icon} />
             </div>
             <div className="hidden transition-all duration-100 group-hover:block">
-                <IoTrashSharp onClick={handleDeleteSubscription} />
+              <IoTrashSharp onClick={() => setDeletePromptOpen(true)} />
             </div>
         </div>
           {/* <button onClick={handleFilter}> */}
             <h3 className="w-max">{sub.title}</h3>
           {/* </button> */}
         </div>
+        <Dialog open={deletePromptOpen} onClose={() => setDeletePromptOpen(false)}>
+          <div className="absolute inset-0 z-50 flex items-center justify-center select-none bg-slate-900/90">
+          <Dialog.Panel as="div" className="p-4 card text-slate-400">
+            <Dialog.Title as="h2" className="text-lg">Remove <span className="font-bold text-slate-300">{sub.title}</span> from your subscriptions?</Dialog.Title>
+            <div className="flex justify-center mt-2 gap-4">
+              <button onClick={handleDeleteSubscription} className="p-1 px-2 bg-red-700 rounded-md">Delete</button>
+              <button onClick={()=>setDeletePromptOpen(false)}className="p-1 px-2 bg-slate-700 rounded-md">Cancel</button>
+            </div>
+          </Dialog.Panel>
+          </div>
+        </Dialog>
+      </>
     );
 }
