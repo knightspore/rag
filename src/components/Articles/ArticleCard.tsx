f4219e81c0df263cf3f2dd08b65c8f1841829613
Ciaran <cslemon96@gmail.com>
2023-02-13 03:06:35 +0200

    fix(ArticleCard): Wrap link around title

--- src/components/Articles/ArticleCard.tsx
+++ src/components/Articles/ArticleCard.tsx
@@ -13,17 +13,17 @@ export default function ArticleCard({
 }) {
   return (
     <div className={`mb-2 p-2 card ${article.is_read && "opacity-30"}`}>
-      <Link prefetch={false} href={`/read/${article.id}`}>
         <div className="text-slate-400">
           <div className="inline-block mr-1 translate-y-[2px]">
             <Icon src={"https://www.google.com/s2/favicons?domain=" + article.url} />
           </div>
           {article.subscription}
         </div>
+      <Link prefetch={false} href={`/read/${article.id}`}>
+        <h2 className="mb-1 text-xl font-medium cursor-pointer">
+          {article.title}
+        </h2>
       </Link>
-      <h2 className="mb-1 text-xl font-medium cursor-pointer">
-        {article.title}
-      </h2>
       <div className="flex items-center gap-2 text-slate-400">
         <LikeButton title={article.title} subscription={article.subscription} />
         <MarkAsReadButton id={article?.id} is_read={article?.is_read || false}  />
